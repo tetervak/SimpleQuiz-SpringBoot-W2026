352e23891c1abb57c0cdfb164f8d7c828bdd8467
Alex Tetervak <tetervak@sheridancollege.ca>
2026-01-12 22:10:15 -0500

    initial commit

---
+++ src/main/java/ca/tetervak/simplequiz/SimpleQuizApplication.java
@@ -0,0 +1,13 @@
+package ca.tetervak.simplequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/tetervak/simplequiz/SimpleQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tetervak.simplequiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
