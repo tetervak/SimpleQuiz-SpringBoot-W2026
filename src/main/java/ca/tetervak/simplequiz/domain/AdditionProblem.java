a0c982aca2c366e34bbff551ad3b641d61baa146
Alex Tetervak <tetervak@sheridancollege.ca>
2026-01-13 06:04:43 -0500

    added implements Serializable

--- src/main/java/ca/tetervak/simplequiz/domain/AdditionProblem.java
+++ src/main/java/ca/tetervak/simplequiz/domain/AdditionProblem.java
@@ -1,8 +1,10 @@
 package ca.tetervak.simplequiz.domain;
 
+import java.io.Serializable;
+
 public record AdditionProblem(
         int firstOperand,
         int secondOperand,
         int correctAnswer
-) {
+) implements Serializable {
 }
