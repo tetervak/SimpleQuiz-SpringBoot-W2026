28fe5f95276484fa2ffa71f75710b0115318a06b
Alex Tetervak <tetervak@sheridancollege.ca>
2026-07-07 14:59:30 -0400

    added bootstrap libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,16 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+
+    // Source: https://mvnrepository.com/artifact/org.webjars/bootstrap
+    implementation("org.webjars:bootstrap:5.3.8")
+
+    // Source: https://mvnrepository.com/artifact/org.webjars/webjars-locator-lite
+    implementation("org.webjars:webjars-locator-lite:1.1.3")
+
+    // Source: https://mvnrepository.com/artifact/org.webjars.npm/bootstrap-icons
+    implementation("org.webjars.npm:bootstrap-icons:1.13.1")
+
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
